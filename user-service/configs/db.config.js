1388f3f23602aed4abc2bf3499a1523246f8d71c
conganhhcmus <conganhhcmus@gmail.com>
2021-05-12 01:14:48 +0700

    update db server

--- user-service/configs/db.config.js
+++ user-service/configs/db.config.js
@@ -1,4 +1,5 @@
-DB_CONNECT_URL="mongodb://conganhhcmus:conganhhcmus@13.59.198.136:27017";
+DB_CONNECT_URL="mongodb://conganhhcmus:conganhhcmus@13.59.198.136:27017/test?authSource=admin";
+// DB_CONNECT_URL="mongodb://conganhhcmus:conganhhcmus@13.59.198.136:27017";
 
 module.exports = {
     mongodbUrl:DB_CONNECT_URL

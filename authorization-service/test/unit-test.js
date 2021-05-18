6fc96c275a0414f1c58936f17abee97edb476ab7
hautran2705 <hautran2705@gmail.com>
2021-05-18 19:36:27 +0700

    Unit test for function loginRoot

--- authorization-service/test/unit-test.js
+++ authorization-service/test/unit-test.js
@@ -1,33 +1,49 @@
 process.env.NODE_ENV = 'test';
 const bcrypt = require("bcryptjs");
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../index')
-
 const connectDB = require("../utils/db");
 const authService = require("../services/auth.service");
-
 const RootUserModel = require("../models/userRoot.model");
 const rootUserFactory = require("../models/factories/userRoot.factory");
 
-const should = chai.should();
-chai.use(chaiHttp);
+var expect = require("chai").expect;
 
 describe('Unit Test for Root User', function() {
     RootUserModel.collection.drop();
-    beforeEach(function(done){
-        var newUser = {
-            username: 'admin',
-            password: bcrypt.hashSync('admin', 10),
-        };
+    var newUser = {
+        username: 'admin',
+        password: bcrypt.hashSync('admin', 10),
+    };
+    RootUserModel.create(newUser);
 
-        RootUserModel.create(newUser);
-        done();
-    });
+    describe('Unit Test for funcion loginRoot', function() {
 
-    afterEach(function(done){
-        RootUserModel.collection.drop();
-        done();
-    });
+        it('test funcion findByUsername', async function(){
+            const user = await rootUserFactory.findByUsername('admin');
+            expect(user.username).to.equal('admin');
+            expect(bcrypt.compareSync('admin' ,user.password)).to.equal(true);
+        });
+    
+        it('test funcion findById', async function(){
+            const uservaild = await rootUserFactory.findByUsername('admin');
+            const user = await rootUserFactory.findById(uservaild._id);
+            expect(user).to.eql(uservaild);
+        });
+    
+        it('test funcion loginRoot true', async function(){
+            const user = await authService.loginRoot({ username: 'admin', password: 'admin'});
+            expect(user).to.have.property('accessToken');
+        });
+
+        it('test funcion loginRoot false username', async function(){
+            const user = await authService.loginRoot({ username: 'admin123', password: 'admin'});
+            expect(user.errorCode).to.eql('login_fail_username');
+        });
+
+        it('test funcion loginRoot false password', async function(){
+            const user = await authService.loginRoot({ username: 'admin', password: 'admin123'});
+            expect(user.errorCode).to.eql('login_fail_password');
+        });
+    
+    })
 })

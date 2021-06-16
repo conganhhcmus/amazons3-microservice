06fd87057b94ce4c0b848d2f635018d29bdcfe0f
a <a>
2021-06-16 10:57:08 +0700

    check service running api gateway

--- api-gateway/routes/api-gateway.js
+++ api-gateway/routes/api-gateway.js
@@ -2,22 +2,23 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-router.all("/", (req, res) => {
-    axios.get("https://authorization-service-s3.herokuapp.com/").then((res) => {
-        console.log(res.data);
-    });
-    axios.get("https://user-service-s3.herokuapp.com/").then((res) => {
-        console.log(res.data);
-    });
-    axios.get("https://storage-service-s3.herokuapp.com/").then((res) => {
-        console.log(res.data);
-    });
-    res.json({
-        message: "API Gateway is running!",
-    });
+router.all("/", async (req, res) => {
+    let auth = await axios.get(
+        "https://authorization-service-s3.herokuapp.com/"
+    );
+    let user = await axios.get("https://user-service-s3.herokuapp.com/");
+    let storage = await axios.get("https://storage-service-s3.herokuapp.com/");
+    let check = {
+        "auth": JSON.stringify(auth.data),
+        "user": JSON.stringify(user.data),
+        "storage": JSON.stringify(storage.data),
+    };
+    console.log(check);
+    res.json(check);
 });
 
 router.all("/api/v1/:service", (req, res) => {
+    console.log(req.params.service);
     res.json({
         message: "API Gateway is running!",
     });

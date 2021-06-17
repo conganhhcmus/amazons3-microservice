d0e7b585bb918d3642f0ca67340fc3d498776c23
SPVN\torres.nguyen <torres.nguyen@starixvn.com>
2021-06-17 23:42:30 +0700

    adding user info when logged

--- authorization-service/services/auth.service.js
+++ authorization-service/services/auth.service.js
@@ -6,97 +6,97 @@ const PERMISSION = require("../constants/permission");
 const { authResponses } = require("../utils/responses");
 
 const userService = {
-    loginRoot,
-    loginIAM,
+  loginRoot,
+  loginIAM,
 };
 
 async function loginRoot(user) {
-    // validate user
+  // validate user
 
-    const { username, password } = user;
+  const { username, password } = user;
 
-    let isUsernameExist = await checkUsernameRootExist(username);
-    if (!isUsernameExist) {
-        return authResponses.loginNotExistUsername();
-    }
+  let isUsernameExist = await checkUsernameRootExist(username);
+  if (!isUsernameExist) {
+    return authResponses.loginNotExistUsername();
+  }
 
-    const userDocument = await rootUserFactory.findByUsername(username);
-    //console.log(userDocument.password);
-    const isValidatePassword = hashingManager.checkValidPassword(
-        password,
-        userDocument.password
-    );
-    if (!isValidatePassword) {
-        return authResponses.loginNotCorrectPassword();
-    }
-    // console.log(userDocument.username)
-    var token = tokenManager.generateAccessToken({
-        userId: userDocument._id,
-        userName:userDocument.username,
-        permission: PERMISSION.FULL_ACCESS,
-    });
+  const userDocument = await rootUserFactory.findByUsername(username);
+  //console.log(userDocument.password);
+  const isValidatePassword = hashingManager.checkValidPassword(
+    password,
+    userDocument.password
+  );
+  if (!isValidatePassword) {
+    return authResponses.loginNotCorrectPassword();
+  }
+  // console.log(userDocument.username)
+  var token = tokenManager.generateAccessToken({
+    userId: userDocument._id,
+    userName: userDocument.username,
+    permission: PERMISSION.FULL_ACCESS,
+  });
 
-    return authResponses.loginSuccess(token);
+  return authResponses.loginSuccess(token, userDocument);
 }
 
 async function loginIAM(user) {
-    // validate user
+  // validate user
 
-    const { username, password, rootUsername } = user;
+  const { username, password, rootUsername } = user;
 
-    let rootUserDocument = await rootUserFactory.findByUsername(rootUsername);
-    if (rootUserDocument === null) {
-        return authResponses.loginNotCorrectOwner();
-    }
-    let isUsernameExist = await checkIAMUsernameExist(
-        username,
-        rootUserDocument.id
-    );
-    if (!isUsernameExist) {
-        return authResponses.loginNotExistUsername();
-    }
+  let rootUserDocument = await rootUserFactory.findByUsername(rootUsername);
+  if (rootUserDocument === null) {
+    return authResponses.loginNotCorrectOwner();
+  }
+  let isUsernameExist = await checkIAMUsernameExist(
+    username,
+    rootUserDocument.id
+  );
+  if (!isUsernameExist) {
+    return authResponses.loginNotExistUsername();
+  }
 
-    const userDocument = await iamUserFactory.findByUsername(username);
+  const userDocument = await iamUserFactory.findByUsername(username);
 
-    const isValidPassword = hashingManager.checkValidPassword(
-        password,
-        userDocument.password
-    );
-    if (!isValidPassword) {
-        return authResponses.loginNotCorrectPassword();
-    }
+  const isValidPassword = hashingManager.checkValidPassword(
+    password,
+    userDocument.password
+  );
+  if (!isValidPassword) {
+    return authResponses.loginNotCorrectPassword();
+  }
 
-    var token = tokenManager.generateAccessToken({
-        userId: userDocument._id,
-        permission: userDocument.permission,
-    });
+  var token = tokenManager.generateAccessToken({
+    userId: userDocument._id,
+    permission: userDocument.permission,
+  });
 
-    return authResponses.loginSuccess(token);
+  return authResponses.loginSuccess(token, userDocument);
 }
 
 async function checkUsernameRootExist(username) {
-    let result = false;
+  let result = false;
 
-    const userDocument = await rootUserFactory.findByUsername(username);
-    if (userDocument) {
-        result = true;
-    }
+  const userDocument = await rootUserFactory.findByUsername(username);
+  if (userDocument) {
+    result = true;
+  }
 
-    return result;
+  return result;
 }
 
 async function checkIAMUsernameExist(username, rootId) {
-    let result = false;
+  let result = false;
 
-    const userDocument = await iamUserFactory.findByUsernameAndOwnerId(
-        username,
-        rootId
-    );
-    if (userDocument) {
-        result = true;
-    }
+  const userDocument = await iamUserFactory.findByUsernameAndOwnerId(
+    username,
+    rootId
+  );
+  if (userDocument) {
+    result = true;
+  }
 
-    return result;
+  return result;
 }
 
 module.exports = userService;

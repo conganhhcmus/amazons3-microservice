4a210c045cc71d4aac3e3156ef73f86dd6d9f113
greent99 <55232248+greent99@users.noreply.github.com>
2021-06-18 13:35:30 +0700

    Update index.js

--- storage-service/routes/index.js
+++ storage-service/routes/index.js
@@ -1,11 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/',async function(req, res, next) {
-  res.render('index', { 
-    title: 'Express',
-   });
+router.get("/", async function (req, res, next) {
+    res.json({
+        message: "API Storage Service is running!",
+    });
 });
 
 module.exports = router;

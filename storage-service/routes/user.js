af8c1b6d70d78a00385092b897f37b5e2f7cb306
greent99 <55232248+greent99@users.noreply.github.com>
2021-06-22 01:16:32 +0700

    ok

--- storage-service/routes/user.js
+++ storage-service/routes/user.js
@@ -5,7 +5,8 @@ const bucketModel = require('../models/bucket.model')
 /* GET home page. */
 router.get('/buckets',async function(req, res, next) {
     const user_id = req.query.user_id || null
-    const buckets = await bucketModel.getByUserId(user_id)
+    const root_id = await bucketModel.getRootByUserId(user_id)
+    const buckets = await bucketModel.getByUserId(root_id)
     return res.json({
         data: buckets
     })

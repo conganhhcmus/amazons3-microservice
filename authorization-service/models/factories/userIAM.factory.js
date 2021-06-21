f28e1eda7a4931c0df995eaccb8ec990c7eb9a6a
SPVN\torres.nguyen <torres.nguyen@starixvn.com>
2021-06-21 09:47:53 +0700

    return full owner info

--- authorization-service/models/factories/userIAM.factory.js
+++ authorization-service/models/factories/userIAM.factory.js
@@ -1,12 +1,15 @@
 const IAMUserModel = require("../userIAM.model");
 
 const IAMUserFactory = {
-    findByUsername: (username) => {
-        return IAMUserModel.findOne({ username });
-    },
-    findByUsernameAndOwnerId: (username, ownerId) => {
-        return IAMUserModel.findOne({ username, owner: ownerId });
-    },
+  findByUsername: (username) => {
+    return IAMUserModel.findOne({ username });
+  },
+  findByUsernameAndOwnerId: (username, ownerId) => {
+    return IAMUserModel.findOne({ username, owner: ownerId }).populate({
+      path: "owner",
+      model: "RootUser",
+    });
+  },
 };
 
 module.exports = IAMUserFactory;

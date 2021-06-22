ff83caa252c73e94f7f419bd712be38e0a4dc341
a <a>
2021-06-22 21:01:20 +0700

    fix storage + add api-gateway

--- storage-service/routes/user.js
+++ storage-service/routes/user.js
@@ -4,8 +4,9 @@ const bucketModel = require('../models/bucket.model')
 
 /* GET home page. */
 router.get('/buckets',async function(req, res, next) {
-    const user_id = req.query.user_id || null
-    const root_id = await bucketModel.getRootByUserId(user_id)
+    const root_id = req.query.root_id || null
+    // const user_id = req.query.user_id || null
+    // const root_id = await bucketModel.getRootByUserId(user_id)
     const buckets = await bucketModel.getByUserId(root_id)
     return res.json({
         data: buckets
